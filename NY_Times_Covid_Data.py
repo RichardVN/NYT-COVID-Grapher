0220014426ea43b1c7c833f9473f74cd66cae028
RichardVN <59857903+RichardVN@users.noreply.github.com>
2020-09-24 16:33:39 -0700

    Refactor menu interface. Add option to clean up .svg files

--- NY_Times_Covid_Data.py
+++ NY_Times_Covid_Data.py
@@ -1,85 +1,38 @@
 """ Retrieves latest COVID-19 data from The New York Times and creates corresponding Histogram graphs.
 
-Data courtesy of The New York Times for non-commercial use.
+Data courtesy of The New York Times for non-commercial use. See LICENSE for mor information.
+Author: Richard Nguyen
 """
 
-from graph_settings import GraphSettings
-from helper_functions import get_NYT_COVID_data, create_histogram
+from helper_functions import get_NYT_COVID_data, graphs_options_menu, get_state_input, svg_cleanup
 
 from text_menu import num_menu
 
-us_covid_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us.csv'
-us_states_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-states.csv'
-us_counties_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/live/us-counties.csv'
-
-states_list = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware',
-               'district of columbia', 'florida', 'georgia', 'guam', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa',
-               'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota',
-               'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'new hampshire', 'new jersey', 'new mexico',
-               'new york', 'north carolina', 'north dakota', 'northern mariana islands', 'ohio', 'oklahoma', 'oregon',
-               'pennsylvania', 'puerto rico', 'rhode island', 'south carolina', 'south dakota', 'tennessee', 'texas',
-               'utah', 'vermont', 'virgin islands', 'virginia', 'washington', 'west virginia', 'wisconsin', 'wyoming']
-
 options_menu = (
-    'Create/Update COVID-19 Cases and Deaths by State',
-    'Create/Update COVID-19 Cases and Deaths by County',
+    'COVID-19 data across all U.S. states',
+    'COVID-19 data for counties in a single state',
+    'Remove .svg graphs from directory',
     'Exit Program')
 
 while True:
-    menu_choice = num_menu(options_menu)
-
+    menu_choice = num_menu('MAIN MENU - Select an Option', options_menu)
+    # Choose get data for States
     if menu_choice == 1:
+        location_type = 'state'
         # retrieve data for States
-        states_last_updated, states_list_by_cases, states_cases, states_list_by_deaths, states_deaths = \
-            get_NYT_COVID_data(us_states_url)
-
-        custom_settings = GraphSettings()
-        # graphs cases per state
-        create_histogram(
-            x_labels=states_list_by_cases,
-            y_label='COVID Cases',
-            y_values=states_cases,
-            title=f'COVID-19 Cases in U.S. States, New York Times ({states_last_updated})',
-            outfname='NYT_COVID_all_states_cases.svg'
-        )
-        # graph deaths per state
-        create_histogram(
-            x_labels=states_list_by_deaths,
-            y_label='COVID Deaths',
-            y_values=states_deaths,
-            title=f'COVID-19 Deaths in U.S. States, New York Times ({states_last_updated})',
-            outfname='NYT_COVID_all_states_deaths.svg'
-        )
-
+        states_data = get_NYT_COVID_data(location_type)
+        # graph data by state
+        graphs_options_menu(states_data, location_type)
+    # Choose get data for counties in a state
     if menu_choice == 2:
+        location_type = 'county'
+        state = get_state_input()
         # retrieve data for counties
-        valid_state = False
-        while not valid_state:
-            state = input('What state would you like to retrieve county data for?: ')
-            if state.lower() in states_list:
-                valid_state = True
-            else:
-                print('That is not a valid U.S. State. Please try again.')
-
-        counties_last_updated, counties_list_by_cases, counties_cases, counties_list_by_deaths, counties_deaths = \
-            get_NYT_COVID_data(us_counties_url, state=state)
-        # graph cases per county
-        create_histogram(
-            x_labels=counties_list_by_cases[0:50],
-            y_label='COVID Cases',
-            y_values=counties_cases[0:50],
-            title=f'COVID-19 Cases in {state.title()} Counties, New York Times ({counties_last_updated})',
-            outfname=f'NYT_COVID_{state}_county_cases.svg'
-        )
-        # graph deaths per county
-        create_histogram(
-            x_labels=counties_list_by_deaths[0:50],
-            y_label='COVID Deaths',
-            y_values=counties_deaths[0:50],
-            title=f'COVID-19 Deaths {state.title()} Counties, New York Times ({counties_last_updated})',
-            outfname=f'NYT_COVID_{state}_county_deaths.svg'
-        )
-
+        counties_data = get_NYT_COVID_data(location_type, state=state)
+        # graph data by county
+        graphs_options_menu(counties_data, location_type, state)
     if menu_choice == 3:
+        svg_cleanup()
+    if menu_choice == 4:
         print('Exiting Program. Thank you! ')
         quit()
